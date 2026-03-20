d3bca22de2771c160213bfb569fd3ece0bdc2970
Raghu K <raghuk.vit@gmail.com>
2026-03-20 08:50:55 +0530

    WMP scripts

---
+++ 07-null-resource-provisioner/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "test" {
+  ami = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-09b871cf63e2bb80b"]
+}
+
+resource "null_resource" "test" {
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      host = aws_instance.test.public_ip
+      user = "ec2-user"
+      password = "DevOps321"
+    }
+    inline = [
+      "echo Hello >/root/some"
+    ]
+  }
+}
+
