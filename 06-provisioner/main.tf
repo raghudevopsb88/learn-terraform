d20798001c31b72033309787a2758d0095e05e71
Raghu K <raghuk.vit@gmail.com>
2026-03-20 08:42:05 +0530

    WMP scripts

---
+++ 06-provisioner/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "test" {
+  ami = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-09b871cf63e2bb80b"]
+
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      host = self.public_ip
+      user = "ec2-user"
+      password = "DevOps321"
+    }
+    inline = [
+      "echo Hello >/root/some"
+    ]
+  }
+}
+
