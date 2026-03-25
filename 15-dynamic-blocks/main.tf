ca0140864ab61c8a66ab09965bbb4573e23d9ffc
Raghu K <raghuk.vit@gmail.com>
2026-03-25 08:02:45 +0530

    WMP scripts

--- 15-dynamic-blocks/main.tf
+++ 15-dynamic-blocks/main.tf
@@ -3,22 +3,6 @@ resource "aws_security_group" "main" {
   for_each = var.component
   name = each.key
 
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
 }
 
 variable "component" {
