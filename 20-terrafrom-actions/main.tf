9ba42b33c5d21b13eb68e37986fdae3644ef119d
Raghu K <raghuk.vit@gmail.com>
2026-03-25 09:01:18 +0530

    WMP scripts

--- 20-terrafrom-actions/main.tf
+++ 20-terrafrom-actions/main.tf
@@ -1,19 +1,39 @@
+action "aws_ec2_stop_instance" "example" {
+  config {
+    instance_id = aws_instance.main.id
+  }
+}
+
 resource "aws_instance" "main" {
   ami           = var.ami
-  instance_type = "t3.large"
+  instance_type = "t3.small"
+
+  lifecycle {
+    # The action_trigger block links the action to lifecycle events
+    action_trigger {
+      events = [
+        "after_create",
+        "after_update"
+      ]
+      actions = [
+        action.aws_ec2_stop_instance.example # Reference the action by its address
+      ]
+    }
+  }
+
 }
 
 variable "ami" {
   default = "ami-0220d79f3f480ecf5"
 }
 
-resource "terraform_data" "restart_trigger" {
-  triggers_replace = [
-    aws_instance.main.ami
-  ]
-
-  provisioner "local-exec" {
-    command = "echo 'Instance ${aws_instance.main.id} was recreated with AMI ${var.ami}'"
-  }
-}
+# resource "terraform_data" "restart_trigger" {
+#   triggers_replace = [
+#     aws_instance.main.ami
+#   ]
+#
+#   provisioner "local-exec" {
+#     command = "echo 'Instance ${aws_instance.main.id} was recreated with AMI ${var.ami}'"
+#   }
+# }
 
