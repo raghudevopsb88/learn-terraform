ff424da5c9e2276dfa5fb823c42ea90a38626d74
Raghu K <raghuk.vit@gmail.com>
2026-03-25 07:53:12 +0530

    WMP scripts

--- 14-splat-expressions/main.tf
+++ 14-splat-expressions/main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "main" {
   for_each = var.servers
   ami = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
+  tags = {
+    Name = each.key
+  }
 }
 
 variable "servers" {
@@ -16,3 +19,6 @@ output "instance_ids" {
   value = [for o in aws_instance.main : o.id]
 }
 
+output "ids_with_name" {
+  value = {for i,j in aws_instance.main : i => j.id }
+}
